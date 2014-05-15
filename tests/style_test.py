3daf64aa7430158845df3b28b497a134130b20bf
berleon <github@leon-sixt.de>
2014-05-16 00:27:01 +0200

    fixed pep8 test
    the pep8 test didn't test the `./tests` directory

--- tests/style_test.py
+++ tests/style_test.py
@@ -4,18 +4,18 @@ from glob import glob
 
 
 class TestCodeFormat(unittest.TestCase):
+    ERROR_MSG = "Please fix you code style errors."
+
     def test_pep8_conformance_core(self):
         files = glob('centerpoints/*.py')
 
         pep8style = pep8.StyleGuide()
         result = pep8style.check_files(files)
-        self.assertEqual(result.total_errors, 0,
-                "Sorry to nitpick :) Please fix you code style errors.")
+        self.assertEqual(result.total_errors, 0, self.ERROR_MSG)
 
     def test_pep8_conformance_tests(self):
-        files = glob('test/*.py')
+        files = glob('tests/*.py')
 
         pep8style = pep8.StyleGuide()
         result = pep8style.check_files(files)
-        self.assertEqual( result.total_errors, 0,
-                "Sorry to nitpick :) Please fix you code style errors.")
+        self.assertEqual(result.total_errors, 0, self.ERROR_MSG)

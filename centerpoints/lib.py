Fix radon_point calculation and add clarkson tests.
radon_points did return a wrong dimension due to matrix -> ndarray transformations (ndarray [1] -> matrix [[1]] -> ndarray [[1]]).

While working on this problem a lot of code has been refactored.

centerpoints/lib.py
@@ -4,11 +4,10 @@ import numpy as np
 
 
 def _find_alphas(points):
-    n_dimensions = len(points[0])
-    n_points = len(points)  # ≙ n
-    n_equations = n_dimensions + 1
+    _points = np.asarray(points)
 
-    equations = np.ones((n_equations, n_points))
+    n_points, n_dimensions = _points.shape
+    n_equations = n_dimensions + 1
 
     # equations = | 1       ...     1   |
     #             | p[1,1]  ...  p[n,1] |
@@ -17,9 +16,9 @@ def _find_alphas(points):
     # (d+1) x n - Matrix
     # where n = n_points and d = n_dimensions
 
-    for dimension in range(n_dimensions):
-        for i in range(n_points):
-            equations[dimension + 1, i] = points[i][dimension]
+    equations = _points.T
+    ones = np.ones(n_points)
+    equations = np.vstack((np.ones(n_points), equations))
 
     # E * a = 0
     U, s, V = np.linalg.svd(equations)
@@ -42,18 +41,25 @@ def radon_point(points):
         where n is the number of points and d the dimension of the points
     Return the radon point as a ndarray.
     """
-    _points = np.array(points)
-    n = len(_points)
-    d = len(_points[0])
+    _points = np.asarray(points)
+    n, d = _points.shape
     assert n >= d + 2
 
     alphas = _find_alphas(_points)
 
     greater_idx = alphas >= 0
-    greater_sum = np.sum(alphas[greater_idx])
-    alphas_g = np.asmatrix(alphas[greater_idx]).T
-    greater_points = np.asmatrix(_points[greater_idx]).T
-    return np.asarray(greater_points * alphas_g / greater_sum)
+    greater_alphas = np.asmatrix(alphas[greater_idx])
+    greater_sum = np.sum(greater_alphas)
+    greater_points = np.asmatrix(_points[greater_idx])
+
+    radon_pt = (greater_alphas / greater_sum) * greater_points
+
+    # Return the radon point as a ndarray and the correct dimension.
+    assert radon_pt.shape == (1, d)
+    radon_pt = np.asarray(radon_pt)
+    radon_pt.shape = d
+
+    return radon_pt
 
 
 def sample_with_replacement(population, k, n=None):
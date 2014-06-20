8809aafb444a6d1ffcc8dea855ce6126b8544c02
jo <kleingeist@googlemail.com>
2014-06-20 17:00:37 +0200

    wip

--- centerpoints/iterated_tverberg.py
+++ centerpoints/iterated_tverberg.py
@@ -1,14 +1,96 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from numpy.math import log, ceil
 
 from .interfaces import CenterpointAlgo
+from .lib import radon_partition
 
 
 class IteratedTverberg(CenterpointAlgo):
 
     def centerpoint(self, points):
-        pass
+        points = np.asrray(points)
+        n, d = points.shape
+
+        # The loop terminates when a point is in the Bucket B_z
+        z = log(ceil(n / (2 * ((d + 1) ** 2))))
+
+        # Initialize empty stacks
+        B = [[] for l in range(z)]
+
+        # Push initial points with trival proofs
+        for s in points:
+            p = [(1, s)]
+            B[0].append((s, [p]))
+
+        while len(B[z]) == 0:
+            # Initialize proof to be an empty stack
+            proof = []
+
+            # Let l be the max such that B_l−1 has at least d + 2 points
+            # ToDO: optimize
+            l = find_l(B, d)
+
+            # Pop d + 2 points q_1 , . . . , q_d+2 from B_l−1
+            qpoints = pop(B[l-1], d + 2)
+
+            # TODO: radon should return gt_idx and lw_idx
+            hull, radon_pt, alphas = radon_partition(qpoints)
+
+            for k in range(2):
+                # TODO: radon should return gt_idx and lw_idx
+                idx = []
+                qpoints_with_proofs = qpoints[idx]
+
+                for i in range(2 ** (l - 1)):
+                    # Let S_ij be the ith part of the proof for q_j
+                    # ?????
+
+                    # TODO: Access the points[gt_idx], points[lw_idx]  which contain proofs
+                    # TODO: union proofs => proofs should be sets
+
+                    # Union of all part i of the proofs.
+                    # X = [q[1][i] for q in qpoints_with_proofs]
+                    # TODO: make this a union
+
+                    # X = [ (a, s), (a, s) ...  ]
+                    X = []
+                    for j, q in enumerate(qpoints_with_proofs):
+                        # q[1][i] = [ (a, s), (a, s) ...  ]
+                        for m in q[1][i]:
+                            X.append(alphas[k][j] * m[0], m[1])
+
+                    # radonpunkt ist nun in abhängigkeit der proofs dargestellt
+                    # TODO: return removed hull point indices from pruning
+                    X2 = _prune2(X)
+                    # X2 = [ (a, s), (a,s) ...  ]
+                    proof.append(X2)
+
+                    # TODO: add the removed hull points do B_0
+
+            B[l].append((radon_pt, proof))
+
+
+
+
+# Let l be the max such that B_l−1 has at least d + 2 points
+def find_l(B, d):
+    l = 0
+    for i, b in B:
+        if len(b) >= d + 2:
+            l = i
+
+    return l + 1
+
+
+def pop(l, n):
+    for i in range(n):
+        yield l.pop()
+
+
+def _prune2(X):
+    pass
 
 
 def _prune(alphas, hull):

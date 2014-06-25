88e9818edcf84e4c12ed6d475a375abc5835d9f9
jo <kleingeist@googlemail.com>
2014-06-25 15:23:10 +0200

    sphere plot nb

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 
+from math import pi, cos, sin
+
 
 def random_sphere_points(n_points, dim):
     assert dim == 3
@@ -15,3 +17,23 @@ def random_sphere_points(n_points, dim):
     z = r * np.cos(theta)
 
     return np.column_stack((x, y, z))
+
+
+def sphere_points(n, r=1):
+    #  assert (n % 2 == 0)
+
+    nn = math.log2(n)
+
+    pi2_steps = np.arange(0, 2*pi, step=(2*pi / nn))
+    pi_steps = np.arange(0, pi, step=(pi / nn))
+
+    points = []
+    for i in pi2_steps:
+        for j in pi_steps:
+            x = r * cos(i) * sin(j)
+            y = r * sin(i) * sin(j)
+            z = r * cos(j)
+            points.append((x, y, z))
+
+    np_points = np.asarray(points)
+    return np_points
